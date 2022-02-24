427ade46ed741f54c8a3e7c4896848715d9678a9
Harry <hareshwarp649>
2022-02-24 23:04:28 +0530

    Decimal to Binnary pro

---
+++ ToBinnary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit
+{
+    internal class ToBinnary
+    {
+        public static void BinnaryTo ()
+        {
+            Console.WriteLine("decimal to binary");
+            Console.WriteLine("enetr number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            List<int> binaryList = new List<int>();
+
+            int power2 = 2;
+            while (num > power2)
+            {
+                power2 *= 2;
+            }
+
+            while (power2 > 0)
+            {
+                
+                
+                if (num >= power2)
+                {
+                    num -= power2;
+                    binaryList.Add(1);
+                }
+                else
+                {
+                    binaryList.Add(0);
+                }
+                power2 /= 2;
+            }
+
+            Console.WriteLine(string.Join(" ", binaryList));
+        }
+    }
+}
