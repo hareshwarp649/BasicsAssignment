cd192862c78b9ae0a0755725bebabd607210e09a
Harry <hareshwarp649>
2022-02-24 22:25:54 +0530

    vending  Machine prog

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+              MachineWedd.WeddMachine();
+
+
+            // k D = new DayOFWeek();
+            //  SetWeek(D, M, Y);
+            // TempConve.ConvertTemp();
+            // MonthPay.PayMonth();
+            //SquareRoot.RootSquare();
+           // ToBinnary.BinnaryTo();
+        }
+    }
+}
